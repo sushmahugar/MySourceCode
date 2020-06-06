4b6a12f4c42fcec62c8546e3e1d29421f26ec570
sushmahugar <66486331+sushmahugar@users.noreply.github.com>
2020-06-06 10:43:27 +0530

    Initail upload

---
+++ NumberToCurrency/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToCurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            string number = Console.ReadLine();
+            string Converteddata = Numbertowords.ConvertAmount(number);
+            Console.WriteLine("converted data is {0}", Converteddata);
+            Console.ReadLine();
+
+        }
+
+           }
+
+    public class Numbertowords
+    {
+        private static String[] units = { "Zero", "One", "Two", "Three",
+    "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+    "Seventeen", "Eighteen", "Nineteen" };
+        private static String[] tens = { "", "", "Twenty", "Thirty", "Forty",
+    "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+        public static string ConvertAmount(string Number)
+        {
+            string words = null;
+            Int64 Numt = int.Parse(Number);
+             words = convert(Numt);
+            return words;
+       }
+
+        public static string convert(Int64 number)
+        {
+            string data = null;
+            if(number < 20)
+            {
+                return  units[number];
+            }
+            if (number < 100)
+            {
+                return tens[number / 10] + ((number % 10 > 0)? " " + convert(number % 10) : " ");
+                
+            }
+            if (number < 1000)
+            {
+                return units[number / 100] + " Hunderd" + ((number % 100 > 0) ? " " + convert(number % 100) : " ");
+            }
+
+            return convert(number / 1000) + " Thousand" + ((number % 1000> 0) ? " " + convert(number % 1000) : " ");
+        }
+    }
+}
